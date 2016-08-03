b269e88155f4ea86099c663912fb3e38d0f3f2d0
qiqig <qiqig@meridianlink.com>
2016-08-03 23:36:45 +0000

    A Chrome developer tool extension that adds a Source Code path pane to the Elements tab. The pane shows the source code path of the inspected element. BugzID: 205839

---
+++ devtools.js
@@ -0,0 +1,57 @@
+﻿function getSourcePath()
+{
+    function getClosestID(element) //The javascript is executed in the inspected page.
+    {
+        var currentElement = element;
+        while (currentElement && !currentElement.id)
+        {
+            currentElement = currentElement.parentNode;
+        }
+
+        if (!currentElement)
+            return "";
+
+        return currentElement.id;
+    }
+
+    var closestID = getClosestID($0);
+    if (!closestID)
+        return "Path not available";
+
+    var longestMatchCount = 0;
+    var sourcePath = "";
+    for (var propertyName in codeMap)
+    {
+        if (closestID.startsWith(propertyName) && propertyName.length > longestMatchCount)
+        {
+            longestMatchCount = propertyName.length;
+            sourcePath = codeMap[propertyName];
+        }
+    }
+
+    if (!sourcePath)
+        return "Path not available";
+    else
+        return sourcePath;
+}
+
+
+chrome.devtools.panels.elements.createSidebarPane("Source Code Path",
+    function (sidebar)
+    {
+        // sidebar initialization code here
+        function updateElementProperties()
+        {
+            //setExpression displays DOM elements and JavaScript objects. Note: not everything is object, eg. the numbers.
+            //sidebar.setExpression("(" + page_getProperties.toString() + ")()","Hello world");
+            //sidebar.setExpression("(" + page_getProperties.toString() + ")()");
+            chrome.devtools.inspectedWindow.eval("(" + getSourcePath + ")()", function (result, exceptionInfo)
+            {
+                sidebar.setPage("pane.html?t=" + encodeURI(result));
+            });
+        }
+
+        updateElementProperties();
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
+    }
+);
---
+++ pane.js
@@ -0,0 +1,15 @@
+﻿function getParameterByName(name, url)
+{
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var path= getParameterByName("t");
+var textBox=document.getElementById("path");
+textBox.value = path;
+textBox.onmouseover = function () { this.select() };
