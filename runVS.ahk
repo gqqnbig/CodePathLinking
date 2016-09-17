389d27b1a9cc9b7acec631b14af1189b2a05ca7a
qiqig <qiqig@meridianlink.com>
2016-09-17 01:56:26 +0000

    Use autohotkey instead as it's unable to hide powershell window. Add registry file. BugzID: 205839

---
+++ runVS.ahk
@@ -0,0 +1,4 @@
+﻿filePath=%1%
+filePath:=SubStr(filePath,4)
+run "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" /edit %filePath%
+exit
--- runVS.ps1
+++
@@ -1,3 +0,0 @@
-$file=$Args[0];
-$file=$file.SubString(3,$file.Length-3);
-& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" /edit $file
